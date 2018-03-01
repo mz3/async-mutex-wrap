38e3e2b9849c7de4a4d571ae15c681e3d4853153
Michael Zandstra <michael@tunnel.ninja>
2018-03-01 10:59:06 -0700

    initial commit

---
+++ mutexify.js
@@ -0,0 +1,67 @@
+// array of unresolved async functions
+const mutex = []
+
+// functional equality comparator
+const functionsEqual = (fn1, fn2) => fn1.toString() === fn2.toString()
+
+// array equality comparator
+const arraysEqual = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+        return false
+    }
+    for (let i = arr1.length; i--;) {
+        if (arr1[i] !== arr2[i]) {
+            return false
+        }
+    }
+
+    return true
+}
+
+// check if mutexed objects {fn, params} are equal
+const checkEquality = (...items) => (
+    functionsEqual(...items.map(item => item.fn))
+    && arraysEqual(...items.map(item => item.params))
+)
+
+// splice item from the mutex array
+const removeMutexItem = item => {
+    const index = mutex.findIndex(mutexItem => checkEquality(mutexItem, item))
+    mutex.splice(index, 1)
+}
+
+// fetch item from the mutex array
+const findMutexItem = item =>
+    mutex.find(mutexItem => checkEquality(mutexItem, item))
+
+// main mutexify function
+module.exports = function mutexify(fn) {
+    return (...params) => {
+        const mutexItem = {
+            fn,
+            params
+        }
+        // check if there is a pending call
+        const foundItem = findMutexItem({ fn, params })
+
+        if (foundItem) {
+            return foundItem.chain
+        } else {
+            // run the function, assume it returns a promise
+            mutexItem.chain = fn(...params)
+                .then(result => {
+                    removeMutexItem(mutexItem)
+                    return result
+                })
+                .catch(err => {
+                    removeMutexItem(mutexItem)
+                    throw err
+                })
+
+            // add this call to the mutex array
+            mutex.push(mutexItem)
+
+            return mutexItem.chain
+        }
+    }
+}
---
+++ mutexify.spec.js
@@ -0,0 +1,56 @@
+const assert = require('assert')
+const mutexify = require('./mutexify')
+const delay = 300
+
+it('mutex should work correctly for a simple function', () => {
+    const fn = mutexify(() => Promise.resolve(1))
+    return fn().then(result => {
+        assert.equal(result, 1)
+    })
+})
+
+it('mutex should work correctly for parallel async calls', () => {
+    let counter = 0
+    const myAsyncFunction = x => new Promise((resolve, reject) => {
+        counter += x
+        setTimeout(resolve, delay, counter)
+    })
+
+    // expect the result of both promise chains to be the same
+    // if not, then ++counter ran twice and the mutex failed
+    const promises = [
+        mutexify(myAsyncFunction),
+        mutexify(myAsyncFunction)
+    ].map(fn =>
+        fn(5).then(result => {
+            assert.equal(result, 5)
+        })
+    )
+
+    return Promise.all(promises)
+})
+
+it('mutex should handle parallel calls, but not non-parallel calls', () => {
+    let counter = 0
+    const myAsyncFunction = () => new Promise((resolve, reject) => {
+        setTimeout(resolve, delay, ++counter)
+    })
+
+    // these 2 calls should run in parallel (return same)
+    const promises = [
+        mutexify(myAsyncFunction),
+        mutexify(myAsyncFunction)
+    ].map(fn =>
+        fn().then(result => {
+            assert.equal(result, 1)
+        })
+    )
+
+    // the third call should run after the first two and not be handled/wrapped
+    // by the mutex, returning 2
+    return Promise.all(promises)
+        .then(() => mutexify(myAsyncFunction)())
+        .then(result => {
+            assert.equal(result, 2)
+        })
+})
